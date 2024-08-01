dc83e0db53f8d96ca5e9c5c650ec4f812168b724
Lucas Keller <kellerjlucas@gmail.com>
2024-07-31 23:29:41 -0600

    RTOS integrated (Bug in e2 studio)
    RZBuddy now has a infinite task, and can run vTaskDelay inside its
    handlers.
    
    Bug:
    The runtime bug I was experiencing was with my Renesas e2 studio instance. I
    restarted, cleared cache, reset hw connections, etc... Something wrong
    with the filesystem caching on e2 end.

--- src/hal_entry.cpp
+++ src/hal_entry.cpp
@@ -6,10 +6,10 @@ FSP_CPP_HEADER
 #include "rzbuddy_thread.h"
 #include "rzbuddy_entry.h"
 // ** RTOS related */
-//void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer,
-//        uint32_t *pulIdleTaskStackSize) BSP_WEAK_REFERENCE;
-//void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer, StackType_t **ppxTimerTaskStackBuffer,
-//        uint32_t *pulTimerTaskStackSize) BSP_WEAK_REFERENCE;
+void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer,
+        uint32_t *pulIdleTaskStackSize) BSP_WEAK_REFERENCE;
+void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer, StackType_t **ppxTimerTaskStackBuffer,
+        uint32_t *pulTimerTaskStackSize) BSP_WEAK_REFERENCE;
 FSP_CPP_FOOTER
 
 
@@ -23,11 +23,10 @@ void hal_entry(void)
 {
     /* Initializes GPT module. */
     handle_error (R_GPT_Open (&g_timer_servo_ctrl, &g_timer_servo_cfg));
-    handle_error (R_GPT_Start (&g_timer_servo_ctrl));
 
-//    rzbuddy_thread_create();
+    rzbuddy_thread_create();
 
-//    vTaskStartScheduler ();
+    vTaskStartScheduler ();
     while (1)
         ;
 }
@@ -55,48 +54,48 @@ void R_BSP_WarmStart(bsp_warm_start_event_t event)
 
 /* Implementation of vApplicationGetIdleTaskMemory() to provide the memory that is
  * used by the Idle task. */
-//void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer,
-//        uint32_t *pulIdleTaskStackSize)
-//{
-//    /* If the buffers to be provided to the Idle task are declared inside this
-//     * function then they must be declared static - otherwise they will be allocated on
-//     * the stack and so not exists after this function exits. */
-//    static StaticTask_t xIdleTaskTCB;
-//    static StackType_t uxIdleTaskStack[ configMINIMAL_STACK_SIZE];
-//
-//    /* Pass out a pointer to the StaticTask_t structure in which the Idle
-//     * task's state will be stored. */
-//    *ppxIdleTaskTCBBuffer = &xIdleTaskTCB;
-//
-//    /* Pass out the array that will be used as the Idle task's stack. */
-//    *ppxIdleTaskStackBuffer = uxIdleTaskStack;
-//
-//    /* Pass out the size of the array pointed to by *ppxIdleTaskStackBuffer.
-//     * Note that, as the array is necessarily of type StackType_t,
-//     * configMINIMAL_STACK_SIZE is specified in words, not bytes. */
-//    *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
-//}
-//
-///* Implementation of vApplicationGetTimerTaskMemory() to provide the memory that is
-// * used by the RTOS daemon/time task. */
-//void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer, StackType_t **ppxTimerTaskStackBuffer,
-//        uint32_t *pulTimerTaskStackSize)
-//{
-//    /* If the buffers to be provided to the Timer task are declared inside this
-//     * function then they must be declared static - otherwise they will be allocated on
-//     * the stack and so not exists after this function exits. */
-//    static StaticTask_t xTimerTaskTCB;
-//    static StackType_t uxTimerTaskStack[ configMINIMAL_STACK_SIZE];
-//
-//    /* Pass out a pointer to the StaticTask_t structure in which the Idle
-//     * task's state will be stored. */
-//    *ppxTimerTaskTCBBuffer = &xTimerTaskTCB;
-//
-//    /* Pass out the array that will be used as the Timer task's stack. */
-//    *ppxTimerTaskStackBuffer = uxTimerTaskStack;
-//
-//    /* Pass out the size of the array pointed to by *ppxTimerTaskStackBuffer.
-//     * Note that, as the array is necessarily of type StackType_t,
-//     * configMINIMAL_STACK_SIZE is specified in words, not bytes. */
-//    *pulTimerTaskStackSize = configMINIMAL_STACK_SIZE;
-//}
+void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer,
+        uint32_t *pulIdleTaskStackSize)
+{
+    /* If the buffers to be provided to the Idle task are declared inside this
+     * function then they must be declared static - otherwise they will be allocated on
+     * the stack and so not exists after this function exits. */
+    static StaticTask_t xIdleTaskTCB;
+    static StackType_t uxIdleTaskStack[ configMINIMAL_STACK_SIZE];
+
+    /* Pass out a pointer to the StaticTask_t structure in which the Idle
+     * task's state will be stored. */
+    *ppxIdleTaskTCBBuffer = &xIdleTaskTCB;
+
+    /* Pass out the array that will be used as the Idle task's stack. */
+    *ppxIdleTaskStackBuffer = uxIdleTaskStack;
+
+    /* Pass out the size of the array pointed to by *ppxIdleTaskStackBuffer.
+     * Note that, as the array is necessarily of type StackType_t,
+     * configMINIMAL_STACK_SIZE is specified in words, not bytes. */
+    *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+}
+
+/* Implementation of vApplicationGetTimerTaskMemory() to provide the memory that is
+ * used by the RTOS daemon/time task. */
+void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer, StackType_t **ppxTimerTaskStackBuffer,
+        uint32_t *pulTimerTaskStackSize)
+{
+    /* If the buffers to be provided to the Timer task are declared inside this
+     * function then they must be declared static - otherwise they will be allocated on
+     * the stack and so not exists after this function exits. */
+    static StaticTask_t xTimerTaskTCB;
+    static StackType_t uxTimerTaskStack[ configMINIMAL_STACK_SIZE];
+
+    /* Pass out a pointer to the StaticTask_t structure in which the Idle
+     * task's state will be stored. */
+    *ppxTimerTaskTCBBuffer = &xTimerTaskTCB;
+
+    /* Pass out the array that will be used as the Timer task's stack. */
+    *ppxTimerTaskStackBuffer = uxTimerTaskStack;
+
+    /* Pass out the size of the array pointed to by *ppxTimerTaskStackBuffer.
+     * Note that, as the array is necessarily of type StackType_t,
+     * configMINIMAL_STACK_SIZE is specified in words, not bytes. */
+    *pulTimerTaskStackSize = configMINIMAL_STACK_SIZE;
+}
--- src/rzbuddy_pwm_manager.c
+++ src/rzbuddy_pwm_manager.c
@@ -14,15 +14,11 @@
 #include "rzbuddy_pwm_manager.h"
 #include "rzbuddy_state.h" /** For accessing globals / consts */
 
-/** TODO: Make compile fail if the servo values are invalid */
-//static const TickType_t SERVO_COMM_PERIOD_MS = 20;
-//static const TickType_t SERVO_PULSE_WIDTH_FAST_PERIOD_MS = 1;
-/** TODO: Experiment with a slower pulse width value */
-//static const TickType_t SERVO_PULSE_WIDTH_SLOW_PERIOD_MS = 1;
+/** TODO: Make compile fail if the servo values are invalid? */
 static const TickType_t SERVO_ROTATION_PERIOD_MS = 600;
-
 /** MIN_NUDGE_TIME_MS tuned arbitrarily */
-//static const TickType_t MIN_NUDGE_TIME_MS = 80;
+static const TickType_t MIN_NUDGE_TIME_MS = 40;
+
 fsp_err_t rzbuddy_dispense()
 {
     fsp_err_t retval = FSP_SUCCESS;
@@ -31,23 +27,7 @@ fsp_err_t rzbuddy_dispense()
     if (retval != FSP_SUCCESS)
         return retval;
 
-//    TickType_t start_tick = xTaskGetTickCount();
-//    TickType_t current_tick = start_tick;
-//    TickType_t ticks_to_pwm = pdMS_TO_TICKS(SERVO_ROTATION_PERIOD_MS);
-
-    /** TODO: Run inside of RTOS task */
     vTaskDelay (pdMS_TO_TICKS(SERVO_ROTATION_PERIOD_MS));
-
-    // software PWM for one rotation
-//    while ((current_tick - start_tick) < ticks_to_pwm)
-//    {
-//        R_BSP_PinWrite(RZBUDDY_SERVO_GPIO, BSP_IO_LEVEL_HIGH);
-//        vTaskDelay(SERVO_PULSE_WIDTH_FAST_PERIOD_MS / portTICK_PERIOD_MS);
-//        R_BSP_PinWrite(RZBUDDY_SERVO_GPIO, BSP_IO_LEVEL_LOW);
-//
-//        vTaskDelay((SERVO_COMM_PERIOD_MS - SERVO_PULSE_WIDTH_FAST_PERIOD_MS) / portTICK_PERIOD_MS);
-//        current_tick = xTaskGetTickCount();
-//    }
 
 // TODO: RESET GPIO HERE? Throttle on the calling side?
     return R_GPT_Stop (&g_timer_servo_ctrl);
@@ -62,10 +42,11 @@ fsp_err_t rzbuddy_pwm_nudge()
     if (retval != FSP_SUCCESS)
         return retval;
 
-    /** This polling isn't necessary if we configure interrupts */
+    // user switch is low on press
     while (R_BSP_PinRead (RZBUDDY_NUDGE_GPIO) == BSP_IO_LEVEL_LOW)
-        // user switch is low on press
-        ;
+    {
+        vTaskDelay (pdMS_TO_TICKS(MIN_NUDGE_TIME_MS));
+    }
 
     return R_GPT_Stop (&g_timer_servo_ctrl);
 }
